1900149969121a94bca68788f929f6c406fc4151
mehdi.byt@gmail.com <mehdi.byt@gmail.com>
2019-02-09 12:19:27 +0330

    some changes

---
+++ Getdescendants.sql
@@ -0,0 +1,30 @@
+﻿
+CREATE PROCEDURE [dbo].[GetDescendantsByParentId]
+        @parentId1 int = null
+        AS
+        BEGIN
+            -- SET NOCOUNT ON added to prevent extra result sets from
+            -- interfering with SELECT statements.
+            SET NOCOUNT ON;
+
+            -- Insert statements for procedure here
+       With
+  NodeCTE (Id, [Name], ParentId)
+  as
+  (
+    Select Id, [Name], ParentId
+    from Node
+    where ParentId = @parentId1
+    
+    union all
+    
+    Select Node.Id, Node.Name, 
+    Node.ParentId
+    from Node
+    join NodeCTE
+    on Node.ParentId = NodeCTE.Id
+  )
+  Select EmpCTE.Id as Id, EmpCTE.Name as Employee, EmpCTE.ParentId as ParentId
+from NodeCTE EmpCTE
+        END
+                    
---
+++ Populatedata_dbo.Node.sql
@@ -0,0 +1,42 @@
+﻿USE [CompanyStructured]
+GO
+
+/****** Object: Table [dbo].[Node] Script Date: 2019/02/08 02:32:48 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+delete from dbo.Node
+
+go
+
+DECLARE @NumberOfRecordsToInsert INT = 5000;
+DECLARE @Inner INT = 1;
+DECLARE @id1 INT = 1;
+DECLARE @name1 nvarchar(70) = 'CEO ';
+DECLARE @parentId1 INT = null;
+SET NOCOUNT ON
+WHILE (@NumberOfRecordsToInsert > 0)
+BEGIN
+    BEGIN TRAN
+      SET @Inner = 0
+      WHILE (@Inner < @NumberOfRecordsToInsert)
+      BEGIN
+	  if(@id1 = 2)
+	  begin
+	    set  @name1 = 'Manager';
+	    set @parentId1 = 1;
+      end	  
+        INSERT INTO [dbo].[Node] ([Id],[Name],[ParentId]) VALUES(@id1,@name1 ,@parentId1);
+        SET @Inner = @Inner+1;
+		set  @name1  = 'stuff ' + convert(nvarchar(30), @id1 + 1);
+	    set @parentId1  = ABS(CHECKSUM(NEWID()) % @id1) + 1;-- generate random parent for given id between 1 to @Id
+		SET @id1 = @id1+1;
+      END
+    COMMIT
+    SET @NumberOfRecordsToInsert = @NumberOfRecordsToInsert - @Inner
+END
+
+
